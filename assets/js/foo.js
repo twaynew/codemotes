630500c3dc6686b73b468fd0dfe35f8fea7630e8
twaynew <114185976+twaynew@users.noreply.github.com>
2023-01-15 23:31:20 -0500

    Update foo.js

--- assets/js/foo.js
+++ assets/js/foo.js
@@ -1,6 +1,6 @@
 function myFunction( myImage ) {
     var time = new Date().getDate();
-    var path = ''.concat("url('", myImage, "')");
+    var path = ''.concat("'url('", myImage, "')'");
 
     if (time % 2 != 0) {
         elem = document.querySelector('.site-header0');
