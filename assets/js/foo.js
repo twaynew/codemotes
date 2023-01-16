7fb8abb757bd973fa2c9af17b565a468e16f0c5b
twaynew <114185976+twaynew@users.noreply.github.com>
2023-01-15 22:18:06 -0500

    Update foo.js

--- assets/js/foo.js
+++ assets/js/foo.js
@@ -1,8 +1,11 @@
 function myFunction( myImage ) {
     var time = new Date().getDate();
     var path = "url(" + myImage + ")";
+    console.log(path);
+    console.log(time % 2);
 
     if (time % 2 != 0) {
         document.querySelector('.site-header0').elem.style.backgroundImage = path;
+        console.log(document.querySelector('.site-header0').elem.style.backgroundImage);
     }
 }
