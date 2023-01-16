8f907a5a9a8768c3d681d044a27f6ea07fa56580
twaynew <114185976+twaynew@users.noreply.github.com>
2023-01-15 21:46:18 -0500

    Update foo.js

--- assets/js/foo.js
+++ assets/js/foo.js
@@ -1,8 +1,6 @@
-function myFunction() {
+function myFunction( myImage ) {
     var time = new Date().getDate();
 
     if (time % 2 != 0) {
-        document.querySelector('.site-header0').elem.style.backgroundImage = "url('/codemotes/assets/Screen Shot 2023-01-13 at 7.03.48 PM.jpg')";
+        document.querySelector('.site-header0').elem.style.backgroundImage = "url(" + myImage + ")";
 }
-
-myFunction();
