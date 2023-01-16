aa7b0ec7035471b27d59fba87a6e99103e7c30a8
twaynew <114185976+twaynew@users.noreply.github.com>
2023-01-16 13:38:17 -0500

    Update foo.js

--- assets/js/foo.js
+++ assets/js/foo.js
@@ -1,17 +1,17 @@
-function myFunction( myImage ) {
-    const imageUrl = "/codemotes/assets/Screen Shot 2023-01-13 at 7.03.48 PM.jpg";
-    
+function myFunction( myImage ) {    
     var time = new Date().getDate();
-    var path = `url('${myImage}')`;
 
     if (time % 2 == 0) {
         elem = document.querySelector('.site-header0');
         let preloaderImg = document.createElement("img");
-        preloaderImg.src = imageUrl;
+        preloaderImg.src = myImage;
         
         preloaderImg.addEventListener('load', (event) => {
-            bgElement.style.backgroundImage = `url(${imageUrl})`;
+            bgElement.style.backgroundImage = `url(${myImage})`;
             preloaderImg = null;
         });
     }
 }
+
+const imageUrl = "/codemotes/assets/Screen Shot 2023-01-13 at 7.03.48 PM.jpg";
+myFunction( imageUrl );
